dcaa75371d8aa69051c99f2d5c6c60baaa719373
b1vth_420 <gazdepat@gmail.com>
2020-06-01 23:39:27 +0200

    Initial commit

---
+++ src/main/java/me/b1vth420/survivalTools/commands/VanishCommand.java
@@ -0,0 +1,4 @@
+package me.b1vth420.survivalTools.commands;
+
+public class VanishCommand {
+}
---
+++ src/main/java/me/b1vth420/survivalTools/listeners/AsyncPlayerChatListener.java
@@ -0,0 +1,13 @@
+package me.b1vth420.survivalTools.listeners;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+
+public class AsyncPlayerChatListener implements Listener {
+
+    @EventHandler
+    public void onChant(AsyncPlayerChatEvent e) {
+
+    }
+}
