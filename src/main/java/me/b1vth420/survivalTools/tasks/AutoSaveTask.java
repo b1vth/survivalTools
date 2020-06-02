8f878fb6a8df955a57f9116ce94e91794ebbd578
b1vth_420 <gazdepat@gmail.com>
2020-06-02 12:08:08 +0200

    A lot of changes

--- src/main/java/me/b1vth420/survivalTools/tasks/AutoSaveTask.java
+++ src/main/java/me/b1vth420/survivalTools/tasks/AutoSaveTask.java
@@ -1,6 +1,9 @@
 package me.b1vth420.survivalTools.tasks;
 
+import me.b1vth420.survivalTools.data.DataSaveType;
+import me.b1vth420.survivalTools.data.configs.Config;
 import me.b1vth420.survivalTools.data.flat.DataManager;
+import me.b1vth420.survivalTools.data.mysql.MySQL;
 import me.b1vth420.survivalTools.managers.UserManager;
 import me.b1vth420.survivalTools.objects.User;
 import org.bukkit.Bukkit;
@@ -10,7 +13,8 @@ public class AutoSaveTask implements Runnable {
     public void run() {
         Bukkit.getOnlinePlayers().forEach((p) -> {
             User u = UserManager.getUser(p);
-            DataManager.saveUser(u);
+            if(Config.getInst().dataSaveType == DataSaveType.FLAT) DataManager.saveUser(u);
+            if(Config.getInst().dataSaveType == DataSaveType.MYSQL) MySQL.getInst().saveAll();
         });
     }
 }
