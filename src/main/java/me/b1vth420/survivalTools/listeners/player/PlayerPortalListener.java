191b88154df19a42c07235f18d52c116854a897d
b1vth_420 <gazdepat@gmail.com>
2020-06-17 19:57:21 +0200

    Logs from detector saved to mysql (only) EntityExplodeListener to disable explosions for example from creeper, or tnt

--- src/main/java/me/b1vth420/survivalTools/listeners/player/PlayerPortalListener.java
+++ src/main/java/me/b1vth420/survivalTools/listeners/player/PlayerPortalListener.java
@@ -9,6 +9,6 @@ public class PlayerPortalListener implements Listener {
 
     @EventHandler
     public void onPortal(PlayerPortalEvent e) {
-        if(Config.getInst().repairNetherPortals) e.setSearchRadius(0);
+        if(Config.getInst().repairNetherPortals) e.setSearchRadius(1);
     }
 }
