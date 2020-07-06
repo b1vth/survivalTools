bd89121b203b4dc6f1975bbf5fed96f6452c40ea
b1vth_420 <gazdepat@gmail.com>
2020-07-06 23:14:42 +0200

    PlayerDeathMessage

--- src/main/java/me/b1vth420/survivalTools/listeners/player/PlayerDeathListener.java
+++ src/main/java/me/b1vth420/survivalTools/listeners/player/PlayerDeathListener.java
@@ -1,5 +1,7 @@
 package me.b1vth420.survivalTools.listeners.player;
 
+import me.b1vth420.survivalTools.data.configs.Messages;
+import me.b1vth420.survivalTools.utils.ChatUtil;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -9,6 +11,10 @@ public class PlayerDeathListener implements Listener {
 
     @EventHandler
     public void onDeath(PlayerDeathEvent e) {
+        e.getEntity().sendMessage(ChatUtil.chat(Messages.getInst().getMessage("playerDeathMessage")
+                .replace("[locationX]", Integer.toString(e.getEntity().getLocation().getBlockX()))
+                .replace("[locationY]", Integer.toString(e.getEntity().getLocation().getBlockY()))
+                .replace("[locationZ]", Integer.toString(e.getEntity().getLocation().getBlockZ()))));
         if(e.getEntity().getKiller() instanceof Player) e.setDeathMessage(null);
     }
 }
